3928df9f395ddcc2872297000721b44d72dce665
Hamed-Ajaj <hamedsgaming@gmail.com>
2026-01-09 14:11:03 +0200

    my simple nvim config

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,15 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'NvChad/nvim-colorizer.lua',
+    event = 'BufReadPre',
+    opts = {
+      user_default_options = {
+        tailwind = true,
+      },
+    },
+  },
+}
